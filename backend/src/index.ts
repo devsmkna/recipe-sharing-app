7611473b86367b57b4fd3ff04ec01cd951eb4839
Davide Visco <davidevisco@email.com>
2024-09-24 22:06:13 +0200

    chore: setup env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
 import cors from "cors";
 import express from "express";
-import { PORT } from "./utils/constants";
+import { NODE_ENV, PORT } from "./utils/constants";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(), express.json());
 
 app.get("/", (_, res) => {
   res.send("Recipe sharing app");
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+  console.log(`PORT ${PORT} | ENV ${NODE_ENV} | Server is running`);
 });
--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,3 +1,18 @@
-// TODO: use cross-env
+import dotenv from "dotenv";
+import env from "env-var";
+import fs from "node:fs";
+import path from "node:path";
 
-export const PORT = process.env.PORT || 3001;
+// get node env
+export const NODE_ENV = env.get("NODE_ENV").required().asString().trim();
+
+// import env file, based on node env
+const envFilePath = path.join(__dirname, "..", "..", `.env.${NODE_ENV}`);
+if (fs.existsSync(envFilePath)) {
+  dotenv.config({
+    path: envFilePath,
+  });
+}
+
+// get env variables
+export const PORT = env.get("PORT").default(4000).asPortNumber();
