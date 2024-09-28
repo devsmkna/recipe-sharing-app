a253bab6fbc2027360e0baed154a03a0f9c56735
Davide Visco <davidevisco@email.com>
2024-09-28 12:37:15 +0200

    feat: add connection to DB on server init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,26 @@
 import cors from "cors";
 import express from "express";
-import { NODE_ENV, PORT } from "./utils/constants";
+import { DB_URI, NODE_ENV, PORT } from "./utils/constants";
+import { connect } from "mongoose";
 
 const app = express();
-
 app.use(cors(), express.json());
+
+const init = async () => {
+  try {
+    await connect(DB_URI);
+    app.listen(PORT, () => {
+      console.log(
+        `PORT ${PORT} | ENV ${NODE_ENV} | Server is running | Connected to database`
+      );
+    });
+  } catch (error) {
+    console.log("An error occured while init the server", error);
+  }
+};
+
+init();
 
 app.get("/", (_, res) => {
   res.send("Recipe sharing app");
-});
-
-app.listen(PORT, () => {
-  console.log(`PORT ${PORT} | ENV ${NODE_ENV} | Server is running`);
 });
