231caf377a3cb7817dc1f03b576a9fd7b09b9b89
Davide Visco <davidevisco@email.com>
2024-09-30 20:05:22 +0200

    fix: add message for each step of signup validation

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -3,17 +3,19 @@ import { body } from "express-validator";
 export const signupValidationChain = [
   body("username")
     .notEmpty()
+    .withMessage("username is required")
     .trim()
-    .toLowerCase()
-    .withMessage("username is required"),
+    .toLowerCase(),
   body("email")
     .notEmpty()
+    .withMessage("email is required")
     .isEmail()
+    .withMessage("email must be a valid email")
     .trim()
-    .toLowerCase()
-    .withMessage("email is required and respect the email format"),
+    .toLowerCase(),
   body("password")
     .notEmpty()
+    .withMessage("password is required")
     .isStrongPassword({
       minSymbols: 0,
       minLength: 8,
@@ -22,6 +24,6 @@ export const signupValidationChain = [
       minUppercase: 1,
     })
     .withMessage(
-      "password is required and must be long at least 8 characters, with at least one lower case character, one upper case and one number."
+      "password must be long at least 8 characters, with at least one lower case character, one upper case and one number."
     ),
 ];
