d33f80e528503bab09b216bf372b7e142a829b39
Davide Visco <davidevisco@email.com>
2024-09-30 20:07:45 +0200

    feat: update index with v1 routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,12 @@ import cors from "cors";
 import express from "express";
 import { DB_URI, NODE_ENV, PORT } from "./utils/constants";
 import { connect } from "mongoose";
+import v1 from "./routes/v1/v1";
 
 const app = express();
 app.use(cors(), express.json());
+
+app.use("/v1", v1);
 
 const init = async () => {
   try {
